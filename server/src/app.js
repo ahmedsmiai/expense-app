ed62fb797fa6bd48a95eb8e4a6988c0b857d6362
ahmedsmiai <47064156+ahmedsmiai@users.noreply.github.com>
2021-09-19 17:23:37 +0100

    sddfs

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require ('morgan');
-const bodyParser = require ('body-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const passport = require('passport')
 const cors = require('cors')
+const path = require('path')
 
 const v1 = require('./routes/v1')
 
@@ -11,14 +12,14 @@ const app = express()
 
 //---------- DB config -----------//
 
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true ,
-    useCreateIndex:true  
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on('connected', () => {
     console.log('Connected to database')
 })
-mongoose.connection.on('error',(err)=>{
+mongoose.connection.on('error', (err) => {
     console.error('failed to connect to database')
 })
 
@@ -30,7 +31,7 @@ app.use(cors())
 
 app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(passport.initialize())
 
@@ -40,17 +41,28 @@ require('./config/passport')(passport)
 
 //---------- Routes -----------//
 
-app.use('/',v1);
+app.use('/', v1);
+
+//---------- Static files -----------//
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(
+        path.resolve(__dirname, '../../client', 'build', 'index.html')
+      );
+    });
+  }
 
 //---------- Errors -----------//
-app.use((req, res, next)=>{
-var err = new Error()
-err.status= 404
-err.message= "Not found url"
-next(err);
+app.use((req, res, next) => {
+    var err = new Error()
+    err.status = 404
+    err.message = "Not found url"
+    next(err);
 })
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
     const status = err.status || 500;
     const error = err.message || 'Error processing url request'
 
